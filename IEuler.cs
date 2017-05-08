83199aa3e300000e56130f2b689172166f304125
CleverChuk <CleverChuk@users.noreply.github.com>
2017-05-08 12:31:29 -0400

    Csharp implementation

---
+++ IEuler.cs
@@ -0,0 +1,16 @@
+﻿/* IEuler.cs
+*
+*  Created on : May 7, 2017
+* Author : CleverChuk
+* Description:  
+*   interface that defines the Euler class contract
+*/
+namespace Euler
+{
+    public delegate double fPointer(double t0, double y0);
+    interface IEuler
+    {        
+        double[] solver(fPointer fptr, double t0, double y0, 
+            double h, double t, int maxIter);
+    }
+}
